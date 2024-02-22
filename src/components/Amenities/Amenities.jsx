c818862b12d5e9d46fcf3dba3e78aa068ed55fc0
sam <samuelabaltz@gmail.com>
2024-02-21 21:40:52 -0900

    Create Location page

--- src/components/Amenities/Amenities.jsx
+++ src/components/Amenities/Amenities.jsx
@@ -1,15 +1,44 @@
-import {Link, Stack, Typography} from "@mui/material";
-
+import {Grid, ImageList, ImageListItem, ImageListItemBar, Link, ListSubheader, Stack, Typography} from "@mui/material";
+import background from "../../photos/hungry-horse-99.jpg";
+import {centerVertHoriz} from "../../global-parameters/Styles";
 function Amenities() {
     return (
-        <Stack sx={{margin: '4rem 4rem'}}>
+        <Stack sx={{margin: '4rem 4rem', ...centerVertHoriz}}>
             <Typography sx={{color: 'black'}}>Amenities</Typography>
-            <Typography sx={{color: 'black'}}>
-                Please visit our <Link>online reservation system</Link> where you can view our current availability,
-                site types, pricing & terms. You can also confirm your reservation and pay for your site through the
-                link below.</Typography>
+            <ImageList sx={{ width: '90vw', height: 450 }} cols={3} rowHeight={164}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img
+                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                        <ImageListItemBar
+                            title={item.title}
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
         </Stack>)
 
 }
 
+
+const itemData = [
+    {
+        img: background,
+        title: 'Breakfast',
+        featured: true,
+    },
+    {
+        img: background,
+        title: 'Breakfast',
+        featured: true,
+    },
+    {
+        img: background,
+        title: 'Breakfast',
+        featured: true,
+    }]
 export default Amenities
--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,5 +1,27 @@
-function Location(){
-    return(<div></div>)
+import background from "../../photos/hungry-horse-99.jpg";
+import {centerVertHoriz} from "../../global-parameters/Styles";
+import {Button, Stack, Typography} from "@mui/material";
+import Box from "@mui/material/Box";
+
+function Location() {
+    return (
+        <Box
+            sx={{
+                backgroundImage: `url(${background})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                width: '100vw',
+                height: '60vh',
+                position: 'relative',
+            }}
+        >
+            <Stack sx={{ margin: '4rem 4rem'}}>
+                <Typography variant={'h3'} sx={{color: 'white'}}>Location</Typography>
+            </Stack>
+        </Box>
+    )
 
 }
+
 export default Location
