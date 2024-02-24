3bbcd09ddf837e26e69c9da1e9900bf27cb952f7
sam <samuelabaltz@gmail.com>
2024-02-24 01:30:28 -0900

    Fix pre nav collapse bug

--- src/components/pre-nav/PreNav.jsx
+++ src/components/pre-nav/PreNav.jsx
@@ -18,16 +18,16 @@ function PreNav() {
         window.location.href = mailToLink;
     };
     return (
-        <Box sx={{flexGrow: 1,}}>
-            <AppBar position="static" sx={{height: isMobile ? '5rem' : '1.7rem', backgroundColor: secondaryColor}}>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static" sx={{ backgroundColor: secondaryColor}}>
                 <Grid container>
-                    <Grid item xs={12} md={2}><Typography sx={{...centerVertHoriz}}><LocalPhoneIcon
+                    <Grid item xs={12} sm={4} md={4}><Typography sx={{...centerVertHoriz}}><LocalPhoneIcon
                         sx={{paddingRight: '.5rem'}}
                         onClick={() => window.open('tel:+14062532867')}/>(406)
                         253-2867</Typography></Grid>
-                    <Grid item xs={12} md={2}><Typography sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem'}}
+                    <Grid item xs={12} sm={4} md={2}><Typography sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem'}}
                                                                                           onClick={handleEmailClick}/>glaciernationalparkcamping@gmail.com</Typography></Grid>
-                    <Grid item xs={12} md={2}><Typography sx={{...centerVertHoriz}}><DirectionsIcon
+                    <Grid item xs={12} sm={4} md={2}><Typography sx={{...centerVertHoriz}}><DirectionsIcon
                         sx={{paddingRight: '.5rem'}}
                         onClick={() => window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu")}/>Directions</Typography></Grid>
                 </Grid>
