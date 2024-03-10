5ee66d0e76728c563099fe2502e6d06bd56f7d1e
Sam Baltz <samuelabaltz@gmail.com>
2024-03-10 10:53:14 -0800

    Update Description.jsx

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -7,21 +7,12 @@ function Description() {
         width: '100vw',
         ...centerVertHoriz
     }}>
-        <Typography sx={{margin: '4rem 4rem'}}>Oceanside RV Park is located in Haines, Alaska right on the shores of the
-            Lynn Canal, North
-            America’s longest fjord. Breathtaking ocean and mountain views surround the park, which is
-            conveniently
-            located adjacent to Main Street, the Haines Harbor, and the downtown business district.
-            As a waterfront campground in Alaska, our guests are constantly spoiled with the incredible views of
-            Alaska’s mountainous terrain and glacial waters. Haines, Alaska is widely known as the adventure
-            capital of
-            Alaska, and Oceanside RV Park provides the perfect location for taking advantage of all our region
-            has to
-            offer.
-
-            Our RV Sites have individual full hook ups (30 & 50 amp electrical, water, sewer), grass pads,
-            picnic
-            tables, and many amenities to make your stay comfortable.</Typography>
+        <Typography sx={{margin: '4rem 4rem'}}>Martin City Park is a convenient, budget-friendly campervan and tent site
+            for travelers and backpack cyclers visiting Glacier National Park, and the Hungry Horse Reservoir areas.
+            Martin City Park is located right off Highway 2 and the entrance into Martin City. The small-acre site
+            boasts the cheapest options for overnight site stays in the area. Included on-site are regularly cleaned
+            portapotties, ground bike racks, and a garbage/recycling bin. Reservations can be made following the
+            link. </Typography>
     </Box>)
 }
 
