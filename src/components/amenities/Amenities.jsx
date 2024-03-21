47bcd47c89d2220c5fdd4ac3318e48a722fd8fb3
Sam Baltz <samuelabaltz@gmail.com>
2024-03-21 11:30:30 -0800

    Correct Image List in Amenities.jsx

--- src/components/amenities/Amenities.jsx
+++ src/components/amenities/Amenities.jsx
@@ -1,21 +1,21 @@
 import {ImageList, ImageListItem, ImageListItemBar, Stack, Typography} from "@mui/material";
 import bikeRack from "../../photos/bike-rack.jpg";
 import trashCan from "../../photos/trash-can.jpg";
-import firePit from "../../photos/fire-pit.jpg";
+import destination from "../../photos/destination.jpg";
 import {centerVertHoriz} from "../../global-parameters/Styles";
 
 function Amenities() {
     return (
-        <Stack sx={{margin: '4rem 4rem', ...centerVertHoriz}}>
+        <Stack sx={{textAlign: 'center', ...centerVertHoriz, marginTop: '3rem', marginBottom: '3rem'}}>
             <Typography sx={{color: 'black'}} variant={'h4'}>Amenities</Typography>
-            <ImageList sx={{width: '90vw',  height: 500}} cols={3} rowHeight={200}>
+            <ImageList sx={{width: '90vw'}} cols={3} rowHeight={200}>
                 {itemData.map((item) => (
                     <ImageListItem key={item.img}>
                         <img
-                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                            src={`${item.img}?w=200&h=200&fit=crop&auto=format`}
                             alt={item.title}
                             loading="lazy"
+                            style={{width: '29.8vw', height: 200}} // Set fixed width and height
                         />
                         <ImageListItemBar
                             title={item.title}
@@ -23,8 +23,8 @@ function Amenities() {
                     </ImageListItem>
                 ))}
             </ImageList>
-        </Stack>)
-
+        </Stack>
+    );
 }
 
 
@@ -40,8 +40,8 @@ const itemData = [
         featured: true,
     },
     {
-        img: firePit,
-        title: 'Fire Pits',
+        img: destination,
+        title: 'Convenient Location',
         featured: true,
     }]
 export default Amenities
