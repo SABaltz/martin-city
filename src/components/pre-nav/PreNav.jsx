4c14e4bff25df6015a14a7def42d138f4b125b3a
Sam Baltz <samuelabaltz@gmail.com>
2024-03-22 09:46:47 -0800

    Update text on prenav to use onclick handlers

--- src/components/pre-nav/PreNav.jsx
+++ src/components/pre-nav/PreNav.jsx
@@ -20,15 +20,13 @@ function PreNav() {
         <Box sx={{flexGrow: 1}}>
             <AppBar position="static" sx={{ backgroundColor: secondaryColor}}>
                 <Grid container>
-                    <Grid item xs={12} sm={4} md={2}><Typography sx={{...centerVertHoriz}}><LocalPhoneIcon
-                        sx={{paddingRight: '.5rem'}}
-                        onClick={() => window.open('tel:+14062532867')}/>(406)
+                    <Grid item xs={12} sm={4} md={2}><Typography onClick={() => window.open('tel:+14062532867')} sx={{...centerVertHoriz}}><LocalPhoneIcon
+                        sx={{paddingRight: '.5rem'}}/>(406)
                         253-2867</Typography></Grid>
-                    <Grid item xs={12} sm={4} md={2}><Typography sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem'}}
-                                                                                          onClick={handleEmailClick}/>glaciernationalparkcamping@gmail.com</Typography></Grid>
-                    <Grid item xs={12} sm={4} md={2}><Typography sx={{...centerVertHoriz}}><DirectionsIcon
-                        sx={{paddingRight: '.5rem'}}
-                        onClick={() => window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu")}/>Directions</Typography></Grid>
+                    <Grid item xs={12} sm={4} md={2}><Typography onClick={handleEmailClick} sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem'}}
+                                                                                        />glaciernationalparkcamping@gmail.com</Typography></Grid>
+                    <Grid item xs={12} sm={4} md={2}><Typography onClick={() => window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu")} sx={{...centerVertHoriz}}><DirectionsIcon
+                        sx={{paddingRight: '.5rem'}}/>Directions</Typography></Grid>
                 </Grid>
             </AppBar>
         </Box>
