8a71520079175966290acb2c953e6ff7d5f07843
Sam Baltz <samuelabaltz@gmail.com>
2024-05-20 09:18:36 -0800

    Update text size

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,13 +1,15 @@
 import {centerVertHoriz} from "../../global-parameters/Styles";
 import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
+import {secondaryColor} from "../../global-parameters/Parameters";
 
 function Description() {
     return (<Box sx={{
         width: '100vw',
-        ...centerVertHoriz
+        ...centerVertHoriz,
+        backgroundColor: secondaryColor
     }}>
-        <Typography sx={{margin: '4rem 4rem'}}>Martin City Park is a convenient, budget-friendly camper van and tent site
+        <Typography sx={{margin: '4rem 4rem', fontSize: '1.5rem'}}>Martin City Park is a convenient, budget-friendly camper van and tent site
             for travelers and backpack cyclers visiting Glacier National Park, and the Hungry Horse Reservoir areas.
             Martin City Park is located right off Highway 2 and the entrance into Martin City. The small-acre site
             boasts the cheapest options for overnight site stays in the area. Included on-site are regularly cleaned
