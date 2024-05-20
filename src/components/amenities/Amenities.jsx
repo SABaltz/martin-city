50e68479004bdee4bb55f8e10cbd7ad5828a5b7c
Sam Baltz <samuelabaltz@gmail.com>
2024-05-20 09:38:32 -0800

    Update Cursor styles for onclick listners

--- src/components/amenities/Amenities.jsx
+++ src/components/amenities/Amenities.jsx
@@ -11,7 +11,9 @@ function Amenities() {
             <Typography sx={{color: 'black'}} variant={'h4'}>Amenities</Typography>
             <ImageList sx={{width: '90vw'}} cols={3} rowHeight={200}>
                 {itemData.map((item) => (
-                    <Box onClick={() => item.title === 'Convenient Location' ? window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu") : ''} >
+                    <Box
+                        onClick={() => item.title === 'Convenient Location' ? window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu") : ''}
+                        sx={{cursor: item.title === 'Convenient Location' ? 'pointer': ''}}>
                         <ImageListItem key={item.img}>
                             <img
                                 src={`${item.img}?w=200&h=200&fit=crop&auto=format`}
--- src/components/pre-nav/PreNav.jsx
+++ src/components/pre-nav/PreNav.jsx
@@ -21,12 +21,12 @@ function PreNav() {
             <AppBar position="static" sx={{ backgroundColor: secondaryColor}}>
                 <Grid container>
                     <Grid item xs={12} sm={4} md={2}><Typography onClick={() => window.open('tel:+14062532867')} sx={{...centerVertHoriz}}><LocalPhoneIcon
-                        sx={{paddingRight: '.5rem'}}/>(406)
+                        sx={{paddingRight: '.5rem', cursor: 'pointer'}}/>(406)
                         253-2867</Typography></Grid>
-                    <Grid item xs={12} sm={4} md={2}><Typography onClick={handleEmailClick} sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem'}}
+                    <Grid item xs={12} sm={4} md={2}><Typography onClick={handleEmailClick} sx={{...centerVertHoriz}}><Mail sx={{paddingRight: '.5rem', cursor: 'pointer'}}
                                                                                         />glaciernationalparkcamping@gmail.com</Typography></Grid>
                     <Grid item xs={12} sm={4} md={2}><Typography onClick={() => window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu")} sx={{...centerVertHoriz}}><DirectionsIcon
-                        sx={{paddingRight: '.5rem'}}/>Directions</Typography></Grid>
+                        sx={{paddingRight: '.5rem', cursor: 'pointer'}}/>Directions</Typography></Grid>
                 </Grid>
             </AppBar>
         </Box>
