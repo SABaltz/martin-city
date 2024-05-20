11d1d5165be5fc4038c554372ec0d47be0575f65
Sam Baltz <samuelabaltz@gmail.com>
2024-05-20 10:37:04 -0800

    Add opacity backdrop to reservations

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -18,23 +18,34 @@ function Reservations() {
             ...centerVertHoriz
         }}
     >
-        <Stack sx={{margin: '4rem 4rem', ...centerVertHoriz}}>
-            <Typography sx={{color: 'white', ...centerVertHoriz, paddingBottom: '1rem'}}
-                        variant={'h3'}>Reservations</Typography>
-            <Typography sx={{color: 'white', fontSize: '1.5rem'}}>
-                Please visit our <Link sx={{color: 'lightBlue'}}>online reservation system</Link> where you can view our
-                current availability,
-                site types, pricing & terms. You can also confirm your reservation and pay for your site through the
-                link above.</Typography>
-            <Button variant={'contained'} sx={{
-                width: '20rem',
-                height: '5rem',
-                backgroundColor: secondaryColor,
-                paddingTop: '1rem'
-            }}><Typography variant={'h5'} sx={{
-                color: 'white',
-            }}>Make A Reservation</Typography></Button>
-        </Stack>
+
+        <Box sx={{
+            backgroundColor: 'rgba(255, 255, 255, 0.13)',
+            borderRadius: '10px',
+            backdropFilter: 'blur(10px)',
+            border: '2px solid rgba(255, 255, 255, 0.1)',
+            boxShadow: '0 0 40px rgba(8, 7, 16, 0.6)',
+        }}>
+            <Stack sx={{margin: '4rem 4rem', ...centerVertHoriz}}>
+
+                <Typography sx={{color: 'white', ...centerVertHoriz, paddingBottom: '1rem'}}
+                            variant={'h3'}>Reservations</Typography>
+                <Typography sx={{color: 'white', fontSize: '1.5rem'}}>
+                    Please visit our <Link sx={{color: 'lightBlue'}}>online reservation system</Link> where you can view
+                    our
+                    current availability,
+                    site types, pricing & terms. You can also confirm your reservation and pay for your site through the
+                    link above.</Typography>
+                <Button variant={'contained'} sx={{
+                    width: '20rem',
+                    height: '5rem',
+                    backgroundColor: secondaryColor,
+                    paddingTop: '1rem',
+                }}><Typography variant={'h5'} sx={{
+                    color: 'white',
+                }}>Make A Reservation</Typography></Button>
+            </Stack>
+        </Box>
     </Box>)
 }
 
