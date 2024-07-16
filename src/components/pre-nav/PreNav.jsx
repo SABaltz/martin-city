a29681d189b556b1305d1e19ce6735070e5c9750
sam <samuelabaltz@gmail.com>
2024-07-16 07:46:14 -0800

    Remove phone number

--- src/components/pre-nav/PreNav.jsx
+++ src/components/pre-nav/PreNav.jsx
@@ -14,9 +14,9 @@ function PreNav() {
         window.location.href = mailToLink;
     };
 
-    const handlePhoneClick = () => {
-        window.open('tel:+14062532867');
-    };
+    // const handlePhoneClick = () => {
+    //     window.open('tel:+14062532867');
+    // };
 
     const handleDirectionsClick = () => {
         window.open("https://www.google.com/maps/place/9234+Hwy+2+E,+Columbia+Falls,+MT+59912/@48.3910565,-114.0436689,17z/data=!3m1!4b1!4m6!3m5!1s0x53663ee5fcea226b:0xa23a1e7f79093ae7!8m2!3d48.391053!4d-114.041094!16s%2Fg%2F11c2h319n3?entry=ttu");
@@ -26,12 +26,12 @@ function PreNav() {
         <Box sx={{flexGrow: 1}}>
             <AppBar position="static" sx={{backgroundColor: secondaryColor, height: '3rem'}}>
                 <Grid container alignItems="center" justifyContent="center">
-                    <Grid item xs={12} sm={4} md={2}>
-                        <Typography onClick={handlePhoneClick} sx={{...centerVertHoriz, cursor: 'pointer'}}>
-                            <LocalPhoneIcon sx={{paddingRight: '.5rem'}}/>
-                            (406) 253-2867
-                        </Typography>
-                    </Grid>
+                    {/*<Grid item xs={12} sm={4} md={2}>*/}
+                        {/*<Typography onClick={handlePhoneClick} sx={{...centerVertHoriz, cursor: 'pointer'}}>*/}
+                        {/*    <LocalPhoneIcon sx={{paddingRight: '.5rem'}}/>*/}
+                        {/*    (406) 253-2867*/}
+                        {/*</Typography>*/}
+                    {/*</Grid>*/}
                     <Grid item xs={12} sm={4} md={2}>
                         <Typography onClick={handleEmailClick} sx={{...centerVertHoriz, cursor: 'pointer'}}>
                             <Mail sx={{paddingRight: '.5rem'}}/>
